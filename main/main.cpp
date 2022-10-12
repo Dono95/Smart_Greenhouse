19b0dc9ec65efc277a14a3b6909e5be908cf6956
Dominik <regec.dominik@gmail.com>
2022-10-12 17:18:27 +0200

    Advertising param

--- main/main.cpp
+++ main/main.cpp
@@ -22,7 +22,67 @@
 #define GREEENHOUSE_PROFILE 0
 
 #define BLUETOOTH_NAME "Greenhouse"
-#define GATTS_AIR_SERVICE_UUID 0x00FF
+
+#define GATTS_AIR_SERVICE_UUID      0x00FF
+#define GATTS_NUM_HANDLE_TEST_A     4
+
+static uint8_t adv_config_done = 0;
+#define adv_config_flag      (1 << 0)
+#define scan_rsp_config_flag (1 << 1)
+
+static uint8_t adv_service_uuid128[32] = {
+    /* LSB <--------------------------------------------------------------------------------> MSB */
+    //first uuid, 16bit, [12],[13] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xEE, 0x00, 0x00, 0x00,
+    //second uuid, 32bit, [12], [13], [14], [15] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,
+};
+
+// The length of adv data must be less than 31 bytes
+//static uint8_t test_manufacturer[TEST_MANUFACTURER_DATA_LEN] =  {0x12, 0x23, 0x45, 0x56};
+//adv data
+static esp_ble_adv_data_t adv_data = {
+    .set_scan_rsp = false,
+    .include_name = true,
+    .include_txpower = false,
+    .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
+    .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(adv_service_uuid128),
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+// scan response data
+static esp_ble_adv_data_t scan_rsp_data = {
+    .set_scan_rsp = true,
+    .include_name = true,
+    .include_txpower = true,
+    //.min_interval = 0x0006,
+    //.max_interval = 0x0010,
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(adv_service_uuid128),
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+
+static esp_ble_adv_params_t adv_params = {
+    .adv_int_min        = 0x20,
+    .adv_int_max        = 0x40,
+    .adv_type           = ADV_TYPE_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    //.peer_addr            =
+    //.peer_addr_type       =
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
 
 /* Declare profile event handler */
 static void gatts_greenhouse_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
@@ -56,6 +116,28 @@ static const char* GATTS_TAG = "Gatt_Server_Greenhouse";
 static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) 
 {
     ESP_LOGI(GATTS_TAG, "%s with type %d", __func__, event);
+
+    switch (event)
+    {
+    case (ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT): {
+        adv_config_done &= (~adv_config_flag);
+        if (adv_config_done == 0){
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+    }
+    case (ESP_GAP_BLE_SCAN_RSP_DATA_SET_COMPLETE_EVT): {
+        adv_config_done &= (~scan_rsp_config_flag);
+        if (adv_config_done == 0){
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+    }
+    default: {
+        ESP_LOGW(GATTS_TAG, "Unhandled event with ID: %d in function %s", event, __func__);
+        break;
+    }
+    }
 }
 
 static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param) 
@@ -102,7 +184,27 @@ static void gatts_greenhouse_profile_event_handler(esp_gatts_cb_event_t event, e
             if (set_dev_name_ret){
                 ESP_LOGE(GATTS_TAG, "set device name failed, error code = %x", set_dev_name_ret);
             }
-           break;
+
+           //config adv data
+            esp_err_t ret = esp_ble_gap_config_adv_data(&adv_data);
+            if (ret){
+                ESP_LOGE(GATTS_TAG, "config adv data failed, error code = %x", ret);
+            }
+            adv_config_done |= adv_config_flag;
+        
+            //config scan response data
+            ret = esp_ble_gap_config_adv_data(&scan_rsp_data);
+            if (ret){
+                ESP_LOGE(GATTS_TAG, "config scan response data failed, error code = %x", ret);
+            }
+            adv_config_done |= scan_rsp_config_flag;
+
+            esp_ble_gatts_create_service(gatts_if, &gl_profile_tab[GREEENHOUSE_PROFILE].service_id, GATTS_NUM_HANDLE_TEST_A);
+            break;
+        }
+        case (ESP_GATTS_CREATE_EVT): 
+        {
+            break;
         }
         default: {
             ESP_LOGW(GATTS_TAG, "Unhandled event with ID: %d", event);
