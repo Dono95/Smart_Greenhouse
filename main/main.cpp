bd3a26aaa2944f78cf78db21a794cdd14503109d
Dominik <regec.dominik@gmail.com>
2022-10-18 15:22:02 +0200

    Create service; add characteristic

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,6 @@
 /* STD includes */
 #include <stdio.h>
+#include <cstring>
 
 /* non-volatile flash memory */
 #include "nvs_flash.h"
@@ -25,6 +26,19 @@
 
 #define GATTS_AIR_SERVICE_UUID      0x00FF
 #define GATTS_NUM_HANDLE_TEST_A     4
+#define GATTS_CHAR_UUID_TEST_A      0xFF01
+
+#define GATTS_DEMO_CHAR_VAL_LEN_MAX 0x40
+
+static uint8_t char1_str[] = {0x11,0x22,0x33};
+static esp_gatt_char_prop_t a_property = 0;
+
+static esp_attr_value_t gatts_demo_char1_val =
+{
+    .attr_max_len = GATTS_DEMO_CHAR_VAL_LEN_MAX,
+    .attr_len     = sizeof(char1_str),
+    .attr_value   = char1_str,
+};
 
 static uint8_t adv_config_done = 0;
 #define adv_config_flag      (1 << 0)
@@ -47,7 +61,7 @@ static esp_ble_adv_data_t adv_data = {
     .include_txpower = false,
     .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
     .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
-    .appearance = 0x00,
+    .appearance = 0xC0,
     .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
     .p_manufacturer_data =  NULL, //&test_manufacturer[0],
     .service_data_len = 0,
@@ -63,7 +77,7 @@ static esp_ble_adv_data_t scan_rsp_data = {
     .include_txpower = true,
     //.min_interval = 0x0006,
     //.max_interval = 0x0010,
-    .appearance = 0x00,
+    .appearance = 0xC0,
     .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
     .p_manufacturer_data =  NULL, //&test_manufacturer[0],
     .service_data_len = 0,
@@ -133,6 +147,22 @@ static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param
         }
         break;
     }
+    case (ESP_GAP_BLE_ADV_START_COMPLETE_EVT): {
+        //advertising start complete event to indicate advertising start successfully or failed
+        if (param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(GATTS_TAG, "Advertising start failed\n");
+        }
+        break;
+    }
+    case (ESP_GAP_BLE_UPDATE_CONN_PARAMS_EVT):
+         ESP_LOGI(GATTS_TAG, "update connection params status = %d, min_int = %d, max_int = %d,conn_int = %d,latency = %d, timeout = %d",
+                  param->update_conn_params.status,
+                  param->update_conn_params.min_int,
+                  param->update_conn_params.max_int,
+                  param->update_conn_params.conn_int,
+                  param->update_conn_params.latency,
+                  param->update_conn_params.timeout);
+        break;
     default: {
         ESP_LOGW(GATTS_TAG, "Unhandled event with ID: %d in function %s", event, __func__);
         break;
@@ -171,45 +201,145 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
 
 static void gatts_greenhouse_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param) 
 {
-    switch (event)
-    {
-        case (ESP_GATTS_REG_EVT): {
-           ESP_LOGI(GATTS_TAG, "Regsiter application with ID [%d] and status %d", param->reg.app_id, param->reg.status); 
-            gl_profile_tab[GREEENHOUSE_PROFILE].service_id.is_primary = true;
-            gl_profile_tab[GREEENHOUSE_PROFILE].service_id.id.inst_id = 0x00;
-            gl_profile_tab[GREEENHOUSE_PROFILE].service_id.id.uuid.len = ESP_UUID_LEN_16;
-            gl_profile_tab[GREEENHOUSE_PROFILE].service_id.id.uuid.uuid.uuid16 = GATTS_AIR_SERVICE_UUID;
-
-            esp_err_t set_dev_name_ret = esp_ble_gap_set_device_name(BLUETOOTH_NAME);
-            if (set_dev_name_ret){
-                ESP_LOGE(GATTS_TAG, "set device name failed, error code = %x", set_dev_name_ret);
-            }
-
-           //config adv data
-            esp_err_t ret = esp_ble_gap_config_adv_data(&adv_data);
-            if (ret){
-                ESP_LOGE(GATTS_TAG, "config adv data failed, error code = %x", ret);
-            }
-            adv_config_done |= adv_config_flag;
+    switch (event) {
+    case ESP_GATTS_REG_EVT: {    
+        ESP_LOGI(GATTS_TAG, "REGISTER_APP_EVT, status %d, app_id %d\n", param->reg.status, param->reg.app_id);
+        gl_profile_tab[GREEENHOUSE_PROFILE].service_id.is_primary = true;
+        gl_profile_tab[GREEENHOUSE_PROFILE].service_id.id.inst_id = 0x00;
+        gl_profile_tab[GREEENHOUSE_PROFILE].service_id.id.uuid.len = ESP_UUID_LEN_16;
+        gl_profile_tab[GREEENHOUSE_PROFILE].service_id.id.uuid.uuid.uuid16 = GATTS_AIR_SERVICE_UUID;
         
-            //config scan response data
-            ret = esp_ble_gap_config_adv_data(&scan_rsp_data);
-            if (ret){
-                ESP_LOGE(GATTS_TAG, "config scan response data failed, error code = %x", ret);
-            }
-            adv_config_done |= scan_rsp_config_flag;
 
-            esp_ble_gatts_create_service(gatts_if, &gl_profile_tab[GREEENHOUSE_PROFILE].service_id, GATTS_NUM_HANDLE_TEST_A);
-            break;
+        esp_err_t set_dev_name_ret = esp_ble_gap_set_device_name(BLUETOOTH_NAME);
+        if (set_dev_name_ret){
+            ESP_LOGE(GATTS_TAG, "set device name failed, error code = %x", set_dev_name_ret);
         }
-        case (ESP_GATTS_CREATE_EVT): 
-        {
-            break;
+
+        //config adv data
+        esp_err_t ret = esp_ble_gap_config_adv_data(&adv_data);
+        if (ret){
+            ESP_LOGE(GATTS_TAG, "config adv data failed, error code = %x", ret);
         }
-        default: {
-            ESP_LOGW(GATTS_TAG, "Unhandled event with ID: %d", event);
-            break;
+        adv_config_done |= adv_config_flag;
+        
+        //config scan response data
+        ret = esp_ble_gap_config_adv_data(&scan_rsp_data);
+        if (ret){
+            ESP_LOGE(GATTS_TAG, "config scan response data failed, error code = %x", ret);
         }
+        adv_config_done |= scan_rsp_config_flag;
+
+        esp_ble_gatts_create_service(gatts_if, &gl_profile_tab[GREEENHOUSE_PROFILE].service_id, GATTS_NUM_HANDLE_TEST_A);
+        break;
+    }
+    case ESP_GATTS_READ_EVT: {
+        ESP_LOGI(GATTS_TAG, "GATT_READ_EVT, conn_id %d, trans_id %d, handle %d\n", param->read.conn_id, param->read.trans_id, param->read.handle);
+        break;
+    }
+    case ESP_GATTS_WRITE_EVT: {
+        ESP_LOGI(GATTS_TAG, "GATT_WRITE_EVT, conn_id %d, trans_id %d, handle %d", param->write.conn_id, param->write.trans_id, param->write.handle);
+        
+        break;
+    }
+    case ESP_GATTS_EXEC_WRITE_EVT:
+        ESP_LOGI(GATTS_TAG,"ESP_GATTS_EXEC_WRITE_EVT");
+        break;
+    case ESP_GATTS_MTU_EVT:
+        ESP_LOGI(GATTS_TAG, "ESP_GATTS_MTU_EVT, MTU %d", param->mtu.mtu);
+        break;
+    case ESP_GATTS_UNREG_EVT:
+        break;
+    case ESP_GATTS_CREATE_EVT: {
+        ESP_LOGI(GATTS_TAG, "CREATE_SERVICE_EVT, status %d,  service_handle %d\n", param->create.status, param->create.service_handle);
+        gl_profile_tab[GREEENHOUSE_PROFILE].service_handle = param->create.service_handle;
+        gl_profile_tab[GREEENHOUSE_PROFILE].char_uuid.len = ESP_UUID_LEN_16;
+        gl_profile_tab[GREEENHOUSE_PROFILE].char_uuid.uuid.uuid16 = GATTS_CHAR_UUID_TEST_A;
+
+        esp_ble_gatts_start_service(gl_profile_tab[GREEENHOUSE_PROFILE].service_handle);
+        a_property = ESP_GATT_CHAR_PROP_BIT_WRITE;
+        esp_err_t add_char_ret = esp_ble_gatts_add_char(gl_profile_tab[GREEENHOUSE_PROFILE].service_handle, &gl_profile_tab[GREEENHOUSE_PROFILE].char_uuid,
+                                                        ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+                                                        a_property,
+                                                        &gatts_demo_char1_val, NULL);
+        if (add_char_ret){
+            ESP_LOGE(GATTS_TAG, "add char failed, error code =%x",add_char_ret);
+        }
+        break;
+    }
+    case ESP_GATTS_ADD_INCL_SRVC_EVT:   
+        break;
+    case ESP_GATTS_ADD_CHAR_EVT: {
+        uint16_t length = 0;
+        const uint8_t *prf_char;
+
+        ESP_LOGI(GATTS_TAG, "ADD_CHAR_EVT, status %d,  attr_handle %d, service_handle %d\n",
+                param->add_char.status, param->add_char.attr_handle, param->add_char.service_handle);
+        gl_profile_tab[GREEENHOUSE_PROFILE].char_handle = param->add_char.attr_handle;
+        gl_profile_tab[GREEENHOUSE_PROFILE].descr_uuid.len = ESP_UUID_LEN_16;
+        gl_profile_tab[GREEENHOUSE_PROFILE].descr_uuid.uuid.uuid16 = ESP_GATT_UUID_CHAR_CLIENT_CONFIG;
+        esp_err_t get_attr_ret = esp_ble_gatts_get_attr_value(param->add_char.attr_handle,  &length, &prf_char);
+        if (get_attr_ret == ESP_FAIL){
+            ESP_LOGE(GATTS_TAG, "ILLEGAL HANDLE");
+        }
+
+        ESP_LOGI(GATTS_TAG, "the gatts demo char length = %x\n", length);
+        for(int i = 0; i < length; i++){
+            ESP_LOGI(GATTS_TAG, "prf_char[%x] =%x\n",i,prf_char[i]);
+        }
+        esp_err_t add_descr_ret = esp_ble_gatts_add_char_descr(gl_profile_tab[GREEENHOUSE_PROFILE].service_handle, &gl_profile_tab[GREEENHOUSE_PROFILE].descr_uuid,
+                                                                ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, NULL, NULL);
+        if (add_descr_ret){
+            ESP_LOGE(GATTS_TAG, "add char descr failed, error code =%x", add_descr_ret);
+        }
+        break;
+    }
+    case ESP_GATTS_ADD_CHAR_DESCR_EVT:
+        gl_profile_tab[GREEENHOUSE_PROFILE].descr_handle = param->add_char_descr.attr_handle;
+        ESP_LOGI(GATTS_TAG, "ADD_DESCR_EVT, status %d, attr_handle %d, service_handle %d\n",
+                 param->add_char_descr.status, param->add_char_descr.attr_handle, param->add_char_descr.service_handle);
+        break;
+    case ESP_GATTS_DELETE_EVT:
+        break;
+    case ESP_GATTS_START_EVT:
+        ESP_LOGI(GATTS_TAG, "SERVICE_START_EVT, status %d, service_handle %d\n",
+                 param->start.status, param->start.service_handle);
+        break;
+    case ESP_GATTS_STOP_EVT:
+        break;
+    case ESP_GATTS_CONNECT_EVT: {
+        esp_ble_conn_update_params_t conn_params = {0};
+        memcpy(conn_params.bda, param->connect.remote_bda, sizeof(esp_bd_addr_t));
+        /* For the IOS system, please reference the apple official documents about the ble connection parameters restrictions. */
+        conn_params.latency = 0;
+        conn_params.max_int = 0x20;    // max_int = 0x20*1.25ms = 40ms
+        conn_params.min_int = 0x10;    // min_int = 0x10*1.25ms = 20ms
+        conn_params.timeout = 400;    // timeout = 400*10ms = 4000ms
+        ESP_LOGI(GATTS_TAG, "ESP_GATTS_CONNECT_EVT, conn_id %d, remote %02x:%02x:%02x:%02x:%02x:%02x:",
+                 param->connect.conn_id,
+                 param->connect.remote_bda[0], param->connect.remote_bda[1], param->connect.remote_bda[2],
+                 param->connect.remote_bda[3], param->connect.remote_bda[4], param->connect.remote_bda[5]);
+        gl_profile_tab[GREEENHOUSE_PROFILE].conn_id = param->connect.conn_id;
+        //start sent the update connection parameters to the peer device.
+        esp_ble_gap_update_conn_params(&conn_params);
+        break;
+    }
+    case ESP_GATTS_DISCONNECT_EVT:
+        ESP_LOGI(GATTS_TAG, "ESP_GATTS_DISCONNECT_EVT, disconnect reason 0x%x", param->disconnect.reason);
+        esp_ble_gap_start_advertising(&adv_params);
+        break;
+    case ESP_GATTS_CONF_EVT:
+        ESP_LOGI(GATTS_TAG, "ESP_GATTS_CONF_EVT, status %d attr_handle %d", param->conf.status, param->conf.handle);
+        if (param->conf.status != ESP_GATT_OK){
+            esp_log_buffer_hex(GATTS_TAG, param->conf.value, param->conf.len);
+        }
+        break;
+    case ESP_GATTS_OPEN_EVT:
+    case ESP_GATTS_CANCEL_OPEN_EVT:
+    case ESP_GATTS_CLOSE_EVT:
+    case ESP_GATTS_LISTEN_EVT:
+    case ESP_GATTS_CONGEST_EVT:
+    default:
+        break;
     }
 }
 
