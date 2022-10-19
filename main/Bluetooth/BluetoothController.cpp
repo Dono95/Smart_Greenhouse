8c8c09963c5f0f2440eb5337691f22675b6cd7d1
Dominik <regec.dominik@gmail.com>
2022-10-19 10:09:04 +0200

    Added constructor and destructor

---
+++ main/Bluetooth/BluetoothController.cpp
@@ -0,0 +1,21 @@
+/* Project specific includes */
+#include "BluetoothController.hpp"
+
+#include <iostream>
+
+using namespace Greenhouse;
+
+/**
+ * @brief Class constructor
+ */
+BluetoothController::BluetoothController()
+{
+    std::cout << "Bluetooth Controller constructor" << std::endl;
+}
+
+/**
+ * @brief Class destructor
+ */
+BluetoothController::~BluetoothController()
+{
+}
---
+++ main/Bluetooth/BluetoothController.hpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of BluetoothController
+ *
+ * @author Dominik Regec
+ */
+#ifndef BLUETOOTH_CONTROLLER
+#define BLUETOOTH_CONTROLLER
+
+namespace Greenhouse
+{
+    class BluetoothController
+    {
+    public:
+        /**
+         * @brief Class constructor
+         */
+        explicit BluetoothController();
+
+        /**
+         * @brief Class destructor
+         */ 
+        ~BluetoothController();
+
+    private:
+    };
+} // namespace Greenhouse
+
+#endif // BLUETOOTH_CONTROLLER
