ac11f6e1c11ca3358f64fe5eae6c30bf0e5a2a7e
Dominik <regec.dominik@gmail.com>
2022-11-29 18:17:34 +0100

    New bluetooth data observer

--- Server/components/Observers/BluetoothDataObserver.hpp
+++ Server/components/Observers/BluetoothDataObserver.hpp
@@ -4,12 +4,25 @@
 /* Common components includes */
 #include "Observer/ObserverInterface.hpp"
 
+/* Project specific includes */
+#include "Managers/EventManager.hpp"
+
 namespace Greenhouse
 {
     namespace Observer
     {
         class BluetoothDataObserver : public Component::Observer::Interface::ObserverInterface
         {
+        public:
+            /**
+             * @brief Class constructor
+             */
+            BluetoothDataObserver(Greenhouse::Manager::EventManager *manager);
+
+            /**
+             * @brief Class destructor
+             */
+            ~BluetoothDataObserver();
         };
     } // namespace Observer
 } // namespace Greenhouse
