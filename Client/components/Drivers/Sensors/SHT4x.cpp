9c0c90e767c366bf14f11c6fd00178f21e278eb4
Dominik <regec.dominik@gmail.com>
2022-12-27 13:33:12 +0100

    Moved I2C to common components.; Moved SHT4x to project specific components;

--- Client/components/Drivers/Sensors/SHT4x.cpp
+++ Client/components/Drivers/Sensors/SHT4x.cpp
@@ -1,9 +1,98 @@
 #include "SHT4x.hpp"
 
+/* ESP log library */
 #include "esp_log.h"
-#include <cmath>
 
 using namespace Sensor;
+
+/**
+ * @brief Class constructor
+ */
+SHT4x::SHT4x(uint8_t i2C_address, Component::Driver::Communication::I2C *i2c)
+    : mI2C_address(i2C_address), mI2C(i2c)
+{
+}
+
+/**
+ * @brief Class destructor
+ */
+SHT4x::~SHT4x()
+{
+}
+
+/**
+ * @brieg Get last measured humanity
+ */
+uint8_t SHT4x::GetHumanity() const
+{
+    return mHumanity;
+}
+
+/**
+ * @brief Get last measured temperature
+ */
+float SHT4x::GetTemperature() const
+{
+    return mTemperature;
+}
+
+/**
+ * @brief Measure temperature and humanity with given precision
+ */
+void SHT4x::Measure(MeasurePrecision precision)
+{
+    /*auto i2c_result = mI2C_I->WriteByte(mI2C_address, static_cast<uint8_t>(precision));
+    if (i2c_result != I2C_OperationResult::WRITE_DATA_SUCCESSFUL)
+    {
+        ESP_LOGE(SHT4x_TAG, "The result of the written command: %s", Interface::EnumToString(i2c_result));
+        return;
+    }*/
+}
+
+/**
+ * @brief Get serial number of the sensor
+ */
+std::string SHT4x::SerialNumber() const
+{
+    auto i2c_result = mI2C->Write(mI2C_address, READ_SERIAL);
+
+    if (i2c_result != I2C_OperationResult::WRITE_DATA_SUCCESSFUL)
+    {
+        ESP_LOGE(SHT4x_TAG, "The result of the written command: %s", I2C_RESULT_TO_STRING(i2c_result));
+        return {};
+    }
+
+    vTaskDelay(1);
+
+    std::vector<uint8_t> data;
+    i2c_result = mI2C->Read(mI2C_address, data, 6);
+
+    if (i2c_result != I2C_OperationResult::READ_DATA_SUCCESSFUL)
+    {
+        ESP_LOGE(SHT4x_TAG, "The result of the reading data: %s", I2C_RESULT_TO_STRING(i2c_result));
+        return {};
+    }
+
+    return std::to_string(data.at(0)) + " " + std::to_string(data.at(1)) + " " + std::to_string(data.at(3)) + " " + std::to_string(data.at(4));
+}
+
+/**
+ * @brief Software reset
+ */
+void SHT4x::SoftReset() const
+{
+    auto i2c_result = mI2C->Write(mI2C_address, SOFT_RESET);
+
+    if (i2c_result != I2C_OperationResult::WRITE_DATA_SUCCESSFUL)
+    {
+        ESP_LOGE(SHT4x_TAG, "The result of the written command: %s", I2C_RESULT_TO_STRING(i2c_result));
+    }
+    vTaskDelay(1);
+}
+
+/*#include "esp_log.h"
+#include <cmath>
+
 
 SHT4x::SHT4x(uint8_t address) : mI2C_Address(address)
 {
@@ -109,4 +198,4 @@ void SHT4x::CalculateValues(const std::vector<uint8_t> &data)
 
     int16_t humanintySensorData = (data.at(3) << 8) | data.at(4);
     mHumanity = (-6 + 125 * (humanintySensorData / (pow(2, 16) - 1)));
-}
+}*/
---
+++ Common_components/Drivers/Communication/I2C.hpp
@@ -0,0 +1,156 @@
+#ifndef I2C_H
+#define I2C_H
+
+/* ESP IDF driver libraries */
+#include "driver/i2c.h"
+
+/* STD library */
+#include <stdint.h>
+#include <vector>
+
+#define I2C_TAG "I2C driver"
+
+// I2C timeout
+#define TIMEOUT_I2C_MS 1000
+//
+#define NUMBER_OF_READ_BYTES_DEFAULT 10
+
+#define I2C_RESULT_TO_STRING(resultValue) Component::Driver::Communication::EnumToString(resultValue)
+
+namespace Component
+{
+    namespace Driver
+    {
+        namespace Communication
+        {
+            enum class I2C_Result
+            {
+                I2C_ERROR,
+                WRITE_DATA_SUCCESSFUL,
+                READ_DATA_SUCCESSFUL,
+                INVALID_ARGUMENT,
+            };
+
+            static const char *EnumToString(I2C_Result value)
+            {
+                switch (value)
+                {
+                case (I2C_Result::I2C_ERROR):
+                    return "I2C_Error";
+                case (I2C_Result::WRITE_DATA_SUCCESSFUL):
+                    return "I2C_Write_Data_Successful";
+                case (I2C_Result::READ_DATA_SUCCESSFUL):
+                    return "I2C_Read_Data_Successful";
+                case (I2C_Result::INVALID_ARGUMENT):
+                    return "I2C_Invalid_Argument";
+                default:
+                    return "Unimplemented value";
+                }
+            }
+
+            class I2C
+            {
+            public:
+                /**
+                 * @brief Class constructor
+                 *
+                 * @param[in] sda   : SDA pin
+                 * @param[in] scl   : SCL pin
+                 */
+                explicit I2C(const uint8_t sda, const uint8_t scl);
+
+                /**
+                 * @brief Class destructor
+                 */
+                ~I2C();
+
+                /**
+                 * Set master mode
+                 *
+                 * @param[in] mode  : Master mode
+                 * @param[in] port  : I2C port
+                 * @param[in] clock : Clock frequency
+                 */
+                void SetMode(i2c_mode_t mode, i2c_port_t port, uint32_t clock);
+
+                /**
+                 * Set slave mode
+                 *
+                 * @param[in] mode                      : Slave mode
+                 * @param[in] port                      : I2C port
+                 * @param[in] enable_10bit_slaveAddress : Enable 10 bit slave address
+                 * @param[in] slaveAddress              : Slave address
+                 * @param[in] maximumSpeed              : Expected clock speed from SCl
+                 */
+                void SetMode(i2c_mode_t mode, i2c_port_t port, uint8_t enable_10bit_slaveAddress,
+                             uint16_t slaveAddress, uint32_t maximumSpeed);
+
+                /**
+                 * @brief Activate I2C driver
+                 *
+                 * @param[in] slave_rx_buffer_length    : Receiving buffer size. Ignored in master mode
+                 * @param[in] slave_tx_buffer_length    : Sending buffer size. Ignored in master mode
+                 * @param[in] interruptFlags            : Flag used to allocate the interrupt
+                 */
+                esp_err_t Activate(size_t slave_rx_buffer_length = 0, size_t slave_tx_buffer_length = 0,
+                                   int interruptFlags = 0) const;
+
+                /**
+                 * @brief Deativate I2C driver
+                 */
+                esp_err_t Deactivate() const;
+
+                /**
+                 * @brief Write data
+                 *
+                 * @param[in] slaveAddress  : Slave address
+                 * @param[in] byte          : Data to send
+                 *
+                 * @return I2C_Result       : WRITE_DATA_SUCCESSFUL - When all data was sent sussessfull
+                 */
+                I2C_Result Write(const uint8_t slaveAddress, const uint8_t byte) const;
+
+                /**
+                 * @brief Write data
+                 *
+                 * @param[in] slaveAddress  : Slave address
+                 * @param[in] data          : Data to send
+                 *
+                 * @return I2C_Result       : WRITE_DATA_SUCCESSFUL - When all data was sent sussessfull
+                 */
+                I2C_Result Write(const uint8_t slaveAddress, std::vector<uint8_t> &data) const;
+
+                /**
+                 * @brief Read data
+                 *
+                 * @param[in] slaveAddress  : Slave address
+                 * @param[out] byte         : Received data
+                 *
+                 * @return I2C_Result       : READ_DATA_SUCCESSFUL - When all data was received sussessfull
+                 */
+                I2C_Result Read(const uint8_t slaveAddress, uint8_t *byte);
+
+                /**
+                 * @brief Read data
+                 *
+                 * @param[in] slaveAddress  : Slave address
+                 * @param[out] data         : Received data
+                 * @param[in] bytesToRead   : Bytes to read
+                 *
+                 * @return I2C_Result       : READ_DATA_SUCCESSFUL - When all data was received sussessfull
+                 */
+                I2C_Result Read(const uint8_t slaveAddress, std::vector<uint8_t> &data, uint8_t bytesToRead);
+
+            private:
+                // Config file of I2C communication interface
+                i2c_config_t mConfig;
+
+                // I2C port number
+                i2c_port_t m_I2C_port;
+            };
+        } // namespace Communication
+
+    } // namespace Driver
+} // namespace Component
+
+#endif /* I2C_H2*/
