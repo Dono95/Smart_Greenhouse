7c4d0f76355fb98f52566aec8efc0560909fb644
Dominik <regec.dominik@gmail.com>
2022-12-31 10:48:04 +0100

    Added MQTT client

--- Server/components/Managers/NetworkManager.cpp
+++ Server/components/Managers/NetworkManager.cpp
@@ -4,10 +4,14 @@
 /* ESP log library*/
 #include <esp_log.h>
 
+#include "sdkconfig.h"
+
 using namespace Greenhouse::Manager;
 
 NetworkManager *NetworkManager::mInstance{nullptr};
 std::mutex NetworkManager::mMutex;
+
+static const char *TAG = "MQTT_EXAMPLE";
 
 /*********************************************
  *              PRIVATE API                  *
@@ -30,6 +34,91 @@ NetworkManager::~NetworkManager()
         delete mWifiDriver;
         mWifiDriver = nullptr;
     }
+}
+
+/**
+ * @brief Callback function when an event occurs
+ */
+void NetworkManager::MQTT_EventHandler(void *handlerArg, esp_event_base_t base,
+                                       int32_t eventID, void *eventData)
+{
+    ESP_LOGD(NETWORK_MANAGER_TAG, "Event dispatched from event loop base=%s, event_id=%d", base, eventID);
+
+    switch (static_cast<esp_mqtt_event_id_t>(eventID))
+    {
+    case (MQTT_EVENT_CONNECTED):
+    {
+        ESP_LOGI(NETWORK_MANAGER_TAG, "MQTT Client is connected to MQTT Broker.");
+        break;
+    }
+    case (MQTT_EVENT_DISCONNECTED):
+    {
+        ESP_LOGI(NETWORK_MANAGER_TAG, "MQTT Client is disconnected from MQTT Broker.");
+        break;
+    }
+    default:
+        ESP_LOGW(NETWORK_MANAGER_TAG, "Unhandled event in switch with ID: %d", eventID);
+    }
+}
+
+/************   MQTT Client     ************/
+/**
+ * @brief Class constructor
+ */
+NetworkManager::MQTT_Client::MQTT_Client(const std::string &uri, const std::string &clientName)
+{
+    // Create clear config structure
+    mConfig = (esp_mqtt_client_config_t *)calloc(1, sizeof(esp_mqtt_client_config_t));
+
+    // Allocate memory and set URI to config structure
+    mConfig->uri = (const char *)malloc(uri.size());
+    mConfig->uri = uri.c_str();
+
+    // Allocate memory and set client name
+    mConfig->client_id = (const char *)malloc(clientName.size());
+    mConfig->client_id = clientName.c_str();
+
+    mClient = esp_mqtt_client_init(mConfig);
+}
+
+/**
+ * @brief Class destructor
+ */
+NetworkManager::MQTT_Client::~MQTT_Client()
+{
+    // Free memory
+    free((char *)mConfig->uri);
+    free((char *)mConfig->client_id);
+
+    free(mConfig);
+
+    esp_mqtt_client_destroy(mClient);
+}
+
+/**
+ * @brief Register MQTT event
+ */
+esp_err_t NetworkManager::MQTT_Client::RegisterEventHandler(esp_mqtt_event_id_t event,
+                                                            esp_event_handler_t eventHandler,
+                                                            void *eventHandlerArg) const
+{
+    return esp_mqtt_client_register_event(mClient, event, eventHandler, eventHandlerArg);
+}
+
+/**
+ * @brief Start MQTT client
+ */
+esp_err_t NetworkManager::MQTT_Client::Start() const
+{
+    return esp_mqtt_client_start(mClient);
+}
+
+/**
+ * @brief Stop MQTT client
+ */
+esp_err_t NetworkManager::MQTT_Client::Stop() const
+{
+    return esp_mqtt_client_stop(mClient);
 }
 
 /*********************************************
@@ -60,4 +149,31 @@ bool NetworkManager::ConnectToWifi(const std::pair<std::string, std::string> &wi
     mWifiDriver->Enable();
 
     return mWifiDriver->Connect();
+}
+
+esp_err_t NetworkManager::ConnectTo_MQTT_Broker(const std::string &uri)
+{
+    if (uri.empty())
+        return ESP_ERR_INVALID_ARG;
+
+    mMQTT_Client = new MQTT_Client("mqtt://192.168.0.104:1883", "ESP_Client");
+    if (mMQTT_Client->RegisterEventHandler(esp_mqtt_event_id_t::MQTT_EVENT_ANY, NetworkManager::MQTT_EventHandler, nullptr))
+    {
+        ESP_LOGE(MQTT_CLIENT_TAG, "Registration event handler failed.");
+        return ESP_FAIL;
+    }
+
+    if (mMQTT_Client->Start())
+    {
+        ESP_LOGE(MQTT_CLIENT_TAG, "Failed to start MQTT client.");
+        return ESP_FAIL;
+    }
+
+    // ESP_LOGE(MQTT_CLIENT_TAG, "IMPORTATNT %s", "TEST_VALUE".);
+    return ESP_OK;
+}
+
+void NetworkManager::Send()
+{
+    ESP_LOGI(TAG, "Test send method");
 }
--- Server/components/Managers/NetworkManager.h
+++ Server/components/Managers/NetworkManager.h
@@ -12,7 +12,13 @@
 /* Project specific includes */
 #include "Observers/BluetoothDataObserver.hpp"
 
+/* ESP MQTT library */
+#include <mqtt_client.h>
+
 #define NETWORK_MANAGER_TAG "Network manager"
+#define MQTT_CLIENT_TAG "MQTT client"
+
+#define DEFAULT_CLIENT_NAME "ESP_Client"
 
 namespace Greenhouse
 {
@@ -38,6 +44,19 @@ namespace Greenhouse
              */
             bool ConnectToWifi(const std::pair<std::string, std::string> &wifiLogin);
 
+            /**
+             * @brief Connect to MQTT broker with provided uri
+             *
+             * @param[in] uri   : MQTT Broker URI
+             *
+             * @return esp_err_t    ESP_OK              : Success
+             *                      ESP_ERR_INVALID_ARG : Empty string in argument
+             *                      ESP_FAIL            : Failure
+             */
+            esp_err_t ConnectTo_MQTT_Broker(const std::string &uri);
+
+            void Send();
+
         private:
             /**
              * @brief Class constructor
@@ -49,19 +68,90 @@ namespace Greenhouse
              */
             ~NetworkManager();
 
-            /* Typedef to WiFi driver component */
+            /**
+             * @brief Callback function when an event occurs
+             *
+             * @param[in] handlerArg    : Pointer to handler arguments
+             * @param[in] base          : Event base
+             * @param[in] eventID       : Event ID
+             * @param[in] eventData     : Event data
+             */
+            static void MQTT_EventHandler(void *handlerArg, esp_event_base_t base,
+                                          int32_t eventID, void *eventData);
+
+            class MQTT_Client
+            {
+            public:
+                /**
+                 * @brief Class constructor
+                 */
+                explicit MQTT_Client(const std::string &uri, const std::string &clientName = DEFAULT_CLIENT_NAME);
+
+                /**
+                 * @brief Class destructor
+                 */
+                ~MQTT_Client();
+
+                /**
+                 * @brief Register MQTT event
+                 *
+                 * @param[in] event             : MQTT event type
+                 * @param[in] eventHandler      : Handler callback
+                 * @param[in] eventHandlerArg   : Handler context data
+                 *
+                 * @return esp_err_t    ESP_OK              : Success
+                 *                      ESP_ERR_INVALID_ARG : Wrong inicialization
+                 *                      ESP_ERR_NO_MEM      : Failed to allocate
+                 */
+                esp_err_t RegisterEventHandler(esp_mqtt_event_id_t event, esp_event_handler_t eventHandler, void *eventHandlerArg) const;
+
+                /**
+                 * @brief Start MQTT client
+                 *
+                 * @return esp_err_t    ESP_OK              : Success
+                 *                      ESP_ERR_INVALID_ARG : Wrong inicialization
+                 *                      ESP_FAIL            : Client is in invalid state
+                 */
+                esp_err_t Start() const;
+
+                /**
+                 * @brief Stop MQTT client
+                 *
+                 * @return esp_err_t    ESP_OK              : Success
+                 *                      ESP_ERR_INVALID_ARG : Wrong inicialization
+                 *                      ESP_FAIL            : Client is in invalid state
+                 */
+                esp_err_t Stop() const;
+
+                void Test()
+                {
+                }
+
+            private:
+                // Pointer to client MQTT configuration file
+                esp_mqtt_client_config_t *mConfig;
+
+                // MQTT client
+                esp_mqtt_client_handle_t mClient;
+            };
+
+            // Typedef to WiFi driver component
             using WifiDriver = Component::Driver::Network::WifiDriver;
 
-            /* Singleton instance of network manager */
+            // Singleton instance of network manager
             static NetworkManager *mInstance;
 
-            /* Singleton mutex to protect instance from multithread*/
+            // Singleton mutex to protect instance from multithread
             static std::mutex mMutex;
 
-            /* Wifi driver */
+            // Wifi driver
             WifiDriver *mWifiDriver;
 
-            /* Data bluetooth  */
+            // MQTT Client
+            MQTT_Client *mMQTT_Client;
+
+            // BluetoothObserver
+            Observer::BluetoothDataObserver *mBluetoothObserver;
         };
     } // namespace Manager
 } // namespace Greenhouse
