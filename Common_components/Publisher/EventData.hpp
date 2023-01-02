bff1b3f2c8f8df69472043851971288098ec2db5
Dominik <regec.dominik@gmail.com>
2023-01-02 19:58:28 +0100

    Added temperature and humanity to BLE event strucutre

--- Common_components/Publisher/EventData.hpp
+++ Common_components/Publisher/EventData.hpp
@@ -25,12 +25,33 @@ namespace Component
             /**
              * @brief Class constructor
              */
-            BluetoothEventData() {}
+            BluetoothEventData(float temperature, uint8_t humanity) : mTemperature(temperature), mHumanity(humanity) {}
 
             /**
              * @brief Class destructor
              */
             ~BluetoothEventData() {}
+
+            /**
+             * @brief Get temperature
+             *
+             * @return float    : Temperature value
+             */
+            float GetTemperature() const { return mTemperature; }
+
+            /**
+             * @brief Get humanity
+             *
+             * @return uint8_t    : Humanity value
+             */
+            uint8_t GetHumanity() const { return mHumanity; }
+
+        private:
+            // Temperature
+            float mTemperature;
+
+            // Humanity
+            uint8_t mHumanity;
         };
     } // namespace Publisher
 } // namespace Greenhouse
